4d1a339fc8037355639feb7b2ec79bd826ab137c
John Dana <john@danasd.com>
2023-03-14 13:13:53 -0600

    useContext, initial implementation.

--- src/About.js
+++ src/About.js
@@ -1,19 +1,26 @@
+import React, { useContext} from 'react';
+import { UserContext } from './UserContext';
 import { Link } from "react-router-dom";
 
-const About = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-    <h1>About</h1>
-  </div>
-);
+const About = () => {
+  const msg = useContext(UserContext);
+  return  (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>About</h1>
+      <div>{msg}</div>
+    </div>
+  );
+    
+}
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import { UserContext } from "./UserContext";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <UserContext.Provider value="Hello from context.">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </div>
   );
 }
