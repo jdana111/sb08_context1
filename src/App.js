50d7b774ae6a610c6fcee51664a4e73f58e6af08
John Dana <john@danasd.com>
2023-03-14 16:04:15 -0600

    Crude implementation of useMemo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
@@ -7,6 +7,8 @@ import { UserContext } from './UserContext';
 
 function App() {
   const [value, setValue] = useState('Hello from context via useState.')
+  // 1) Text-in const providerValue, 2) Swap UserContext.Provider value={providerValue}.
+  // const providerValue = useMemo(() => ({ value, setValue }), [value, setValue])
   return (
     <div className="App">
         <UserContext.Provider value={{ value, setValue }}>
