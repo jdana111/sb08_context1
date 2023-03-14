c19f28accc8d7f36c15b27af63ea3199bf948707
John Dana <john@danasd.com>
2023-03-14 13:55:24 -0600

    Push useContext(UserContext) to Contact and Home components

--- src/Contact.js
+++ src/Contact.js
@@ -1,19 +1,25 @@
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 import { Link } from "react-router-dom";
 
-const Contact = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    <h1>Contact</h1>
-  </div>
-);
+const Contact = () => {
+  const { value, setValue } = useContext(UserContext);
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>Contact</h1>
+      <div>{value}</div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,25 @@
+import react, { useContext } from "react";
+import { UserContext } from "./UserContext";
 import { Link } from "react-router-dom";
 
-const Home = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-    <h1>Home</h1>
-  </div>
-);
+const Home = () => {
+  const { value, setValue } = useContext(UserContext);
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>Home</h1>
+      <div>{value}</div>
+    </div>
+  );
+};
 
 export default Home;
